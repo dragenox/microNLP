501b3a2e3a1e5bb52eb243023cd0fa5e7084948d
Dragenox <ishan.d.gupta@outlook.com>
2020-02-18 17:34:00 +0530

    Initialize Repository
    Adding files to repository

---
+++ micronlp/src/main/java/com/rcpit/g36fyp1920/micronlp/microNLP.java
@@ -0,0 +1,10 @@
+package com.rcpit.g36fyp1920.micronlp;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class microNLP {
+    public static void summary(Context c, String message) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
+}
