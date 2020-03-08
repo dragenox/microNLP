16a2a14f5b68b78ab5850e07a292bef66967d5e0
Dragenox <ishan.d.gupta@outlook.com>
2020-03-08 21:35:48 +0530

    Added Database Helper and database

--- micronlp/src/main/java/com/rcpit/g36fyp1920/micronlp/microNLP.java
+++ micronlp/src/main/java/com/rcpit/g36fyp1920/micronlp/microNLP.java
@@ -1,10 +1,47 @@
 package com.rcpit.g36fyp1920.micronlp;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 public class microNLP {
-    public static void test(Context c) {
-        Toast.makeText(c, "Library Configured", Toast.LENGTH_SHORT).show();
+    private Context context;
+
+    public microNLP(Context context){
+        this.context = context;
+    }
+    public void test() {
+        Toast.makeText(context, "Library Configured", Toast.LENGTH_SHORT).show();
+        initDatabase();
+    }
+
+    private void initDatabase() {
+        DatabaseHelper myDbHelper = new DatabaseHelper(context);
+        try {
+            myDbHelper.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+        myDbHelper.openDataBase();
+        Toast.makeText(context, "Successfully Imported", Toast.LENGTH_SHORT).show();
+        Cursor cursor = myDbHelper.query("microNLP", null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Toast.makeText(context,
+                        "_id: " + cursor.getString(0) + "\n" +
+                                "data: " + cursor.getString(1) + "\n" +
+                                "link: " + cursor.getString(2) + "\n" +
+                                "tags:  " + cursor.getString(3)+ "\n" +
+                                "severity:  " + cursor.getString(4),
+                        Toast.LENGTH_LONG).show();
+            } while (cursor.moveToNext());
+        }
+    }
+
+    public String summary(String text) {
+        return text;
     }
 }
