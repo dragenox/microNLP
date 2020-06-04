41a92e32311d8c874d18af874cada61b5676a49d
Dragenox <ishan.d.gupta@outlook.com>
2020-06-04 17:25:54 +0530

    Last major changes to NLP

--- micronlp/src/main/java/com/rcpit/g36fyp1920/micronlp/DatabaseHelper.java
+++ micronlp/src/main/java/com/rcpit/g36fyp1920/micronlp/DatabaseHelper.java
@@ -3,19 +3,12 @@ package com.rcpit.g36fyp1920.micronlp;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Build;
-import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Objects;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
